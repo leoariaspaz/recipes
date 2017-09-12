942e88ee8c868dd2bc94f37554a1461a72431391
Leonardo Arias Paz <larias@bse.com.ar>
2017-09-12 14:26:33 -0300

    Handle multiple models in one form
    Múltiples modelos en 1 formulario

--- app/assets/javascripts/projects.coffee
+++ app/assets/javascripts/projects.coffee
@@ -1,3 +1,8 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+ready = ->
+	$('.nav_link').click ->
+    false
+
+$(document).on("page:load", ready)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+	def error_messages_for(*params)
+		options = params.extract_options!.symbolize_keys
+		objects = params.collect { |name| instance_variable_get("@#{name}") }
+		error_messages = objects.map { |o| o.full_messages }
+		unless error_messages.flatten!.empty?
+			if options[:partial]
+				render partial: options[:partial], locals: { errors: error_messages }
+			else
+				header = "Se produjeron los siguientes errores:"
+				error_list = error_messages.map { |m| content_tag(:li, m) }
+				contents = ''
+				contents << content_tag(:h2, header)
+				contents << content_tag(:ul, error_list)
+				content_tag(:div, contents, class: 'errorExplanation', id: 'errorExplanation')
+			end
+		else
+			''
+		end
+	end
 end
